3a64b8daa0e0a7ad0f5ff357ca01e71ef9f508d9
Vterebenin <niceserver11@mail.ru>
2018-05-03 18:24:50 +0300

    Add pages to App

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 		resources :comments
 	end
 	root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	get "/about", to: 'pages#about'  
 end
